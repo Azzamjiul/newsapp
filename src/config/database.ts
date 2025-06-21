43dbae0789be863581af30e12a6d4c31dd1f80eb
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2025-06-21 22:27:13 +0700

    feat: add UserDeviceToken model to Sequelize configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 import { config } from './index';
 import { User } from '../domain/user/user.model';
 import { News } from '../domain/news/news.model';
+import { UserDeviceToken } from '../domain/user/userDeviceToken.model';
 
 const sequelize = new Sequelize({
   database: config.db.name,
@@ -10,7 +11,7 @@ const sequelize = new Sequelize({
   password: config.db.password,
   host: config.db.host,
   port: config.db.port,
-  models: [User, News],
+  models: [User, News, UserDeviceToken],
   logging: false,
 });
 
